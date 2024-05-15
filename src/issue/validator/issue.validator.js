769fd22fb56f38f95aa7e8d64aa193fdad1eaaa4
pharaoh <pharaohmanson@gmail.com>
2024-05-15 14:30:10 -0400

    issue validator

--- src/issue/validator/issue.validator.js
+++ src/issue/validator/issue.validator.js
@@ -2,16 +2,10 @@ class IssueValidator{
 
     validateInputString( body ) {
 
-        if(body == {}) throw new Error('Input must be a string');
-        
         for (const [key, value] of Object.entries(body)) {
-            
-            if (typeof value !== 'string') {
-                throw new Error('Input must be a string');
-            }
     
             if (value === null || value === undefined || value === '')  {
-                throw new Error('Input must not be null or undefined');
+                throw new Error(` The "${key}" input is required`);
             }
     
         }

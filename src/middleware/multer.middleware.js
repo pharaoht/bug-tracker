01b6084dc82082619a489906054f535c5ed4b1cc
pharaoh <pharaohmanson@gmail.com>
2024-05-30 11:43:55 -0400

    update path for saving files

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
 
-        cb(null, 'images/');
+        cb(null, path.join(__dirname, '../../images'));
     },
 
     filename: function (req, file, cb) {
